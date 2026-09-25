3a8a164e2d3194b441a9538dba161eee22e752b5
Anish <anishgupta1789@gmail.com>
2026-09-25 23:41:05 +0530

    Update cookie settings for secure and sameSite attributes

--- Backend/controller/authController.js
+++ Backend/controller/authController.js
@@ -37,8 +37,8 @@ export const register = async (req, res) => {
         const token = generateToken(user._id);
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
         });
         res.status(201).json({
@@ -92,8 +92,8 @@ export const login = async (req, res) => {
 
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
@@ -146,8 +146,8 @@ export const googleLogin = async (req, res) => {
 
         res.cookie("token", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
@@ -183,8 +183,8 @@ export const adminLogin = async (req, res) => {
         const token = generateAdminToken(email);
         res.cookie("adminToken", token, {
             httpOnly: true,
-            secure: false,
-            sameSite: "strict",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
         res.status(200).json(token);
