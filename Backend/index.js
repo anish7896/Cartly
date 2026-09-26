b9fb134ee043b83835f83d97014b239542c23139
Anish <anishgupta1789@gmail.com>
2026-09-26 11:07:54 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["https://cartly-frontend-qvyy.onrender.com", "http://localhost:5174"],
     credentials: true
 }));
 
