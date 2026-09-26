e24d5fb41558e73494aaff5a8fdfb7169562c2ef
Anish <anishgupta1789@gmail.com>
2026-09-26 10:53:15 +0530

    Update Register.jsx

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -8,7 +8,7 @@ import { useState, useContext } from "react";
 import { AuthContext } from "../context/authContext";
 import axios from "axios";
 
-import { auth, provider } from "../utils/Firebase.js";
+import { auth, provider } from "../../utils/Firebase.js";
 import { signInWithPopup } from "firebase/auth";
 
 import { useUser } from "../context/UserContext";
